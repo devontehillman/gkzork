68c57637393b3be8fcd59289a70ff1d532f583dc
devontehillman <hillmand@mail.gvsu.edu>
2025-02-22 19:25:18 -0500

    new main

--- main.cpp
+++ main.cpp
@@ -1,43 +1,26 @@
 #include <iostream>
 #include <string>
 #include "Item.hpp"
-
-class NPC {
-private:
-    std::string name;
-    std::string description;
-    int messageNumber;
-    // messages;
-
-public:
-    //  initializer list constructor
-    NPC(std::string npcName, std::string description)
-    // Initializer List:   
-    : name(npcName), description(description){}
-
-    // Getter function for name
-    std::string getName() const { return name; }
-
-     // Getter function for description
-     std::string getDescription() const { return description; }
-
-    // Overload << operator as a friend function
-    friend std::ostream& operator<<(std::ostream& os, const NPC& npc) {
-        os << npc.name, npc.description;
-        return os;
-    }
-};
+#include "npc.hpp"
 
 int main() {
-    NPC npc1("Guard", "Halt! Who goes there?");
     Item booze("Booze", "Drink your pain away", 215, 11.75);
 
-    
+    // Creating an NPC with multiple messages
+    NPC npc1("Guard", "A stern-looking watchman.", {"Halt! Who goes there?", "Stay out of trouble.", "Move along."});
+
+    // Display NPC name and description using overloaded <<
+    std::cout << npc1 << std::endl;
+
+    // Getting messages one by one
+    std::cout << "NPC says: " << npc1.getNextMessage() << std::endl;
+    std::cout << "NPC says: " << npc1.getNextMessage() << std::endl;
+    std::cout << "NPC says: " << npc1.getNextMessage() << std::endl;
+    std::cout << "NPC says: " << npc1.getNextMessage() << std::endl; // Loops back to first message
+
     // Using overloaded << operator
     std::cout << "NPC Name: " << npc1 << booze << std::endl;
     std::cout << "Item here: " << booze << std::endl;
-
-
 
     return 0;
 }
