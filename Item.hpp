f5d544ee7d8fa941acdc50d4097edfebfd2d1503
Horacio Vasquez <vasqueho@mail.gvsu.edu>
2025-03-02 18:30:09 -0500

    Cleaned up the code by removing unnecessary comments

--- Item.hpp
+++ Item.hpp
@@ -51,59 +51,13 @@ class Item {
 
     public:
     Item(const std::string& itemname, const std::string& descriptname, int calories, float weight) {
-        // if (itemname == "") {
-        //     throw std::invalid_argument("item name is blank, fix this now!");
-        // }
-        // else {
-        //     this->itemname = item_name;
-        // }
+
         set_itemname(itemname);
         set_itemdescription(descriptname);
         set_itemcalories(calories);
         set_itemweight(weight);
-
-
-        // this->itemname = itemname;
-        // this->descriptname = descriptname;
-        // this->calories = calories;
-        // this->weight = weight;
     }
 
-    // void set_itemname(std::string item_name) {
-    //     if (item_name == "") {
-    //         throw std::invalid_argument("item name is blank, fix this now!");
-    //     }
-    //     else {
-    //         this->itemname = item_name;
-    //     }
-    // }
-
-    // void set_itemcalories(int item_calories) {
-    //     if (item_calories < 0 || item_calories > 1000) {
-    //         throw std::runtime_error("calories of item is not between 0-1000");
-    //     }
-    //     else {
-    //         calories = item_calories;
-    //     }
-    // }
-
-    // void set_itemdescription(std::string item_descript) {
-    //     if (item_descript == "") {
-    //         throw std::runtime_error("description of item is blank, fix this now!");
-    //     }
-    //     else {
-    //         descriptname = item_descript;
-    //     }
-    // }
-
-    // void set_itemweight(float item_weight) {
-    //     if (item_weight < 0 || item_weight > 500) {
-    //         throw std::runtime_error("weight of item is not between 0-500");
-    //     }
-    //     else {
-    //         weight = item_weight;
-    //     }
-    // }
     std::string get_name() const {
         return itemname;
     }
@@ -122,9 +76,9 @@ class Item {
     }
 
     friend std::ostream& operator<<(std::ostream& os, const Item& obj){
-    os << obj.itemname << "(" << obj.calories << " calories) - " << obj.weight << " lb - " << obj.descriptname;
-    // os << obj.itemname << "description: " << obj.descriptname << "calories " 
-    // << obj.calories << " weight: " << obj.weight;
+        
+    os << obj.itemname << "(" << obj.calories << " calories) - " << obj.weight << 
+    " lb - " << obj.descriptname;
     return os;
     };
 
