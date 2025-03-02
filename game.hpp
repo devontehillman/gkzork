6bf265389905cb95542b8e0f8fd31852670a5114
devontehillman <hillmand@mail.gvsu.edu>
2025-03-01 19:06:41 -0500

    calorie count commented out functions that are not created yet

--- game.hpp
+++ game.hpp
@@ -19,7 +19,7 @@ class Game {
     int weight;
     std::vector<Location> world;
     Location currentLocation;
-    int numCalories = 0;
+    int numCalories = 500;
     bool gameActive;
     
     public:
@@ -28,14 +28,13 @@ class Game {
     
     // Forward declaration of actions
     void show_help(const std::vector<std::string>& target);
-    void meet(const std::vector<std::string>& target); //Hrc
+    //void meet(const std::vector<std::string>& target); //Hrc
     void take(const std::vector<std::string>& target); //xx
     void give(const std::vector<std::string>& target); //xx
-    void go(const std::vector<std::string>& target); //Hrc
-    void show_items(const std::vector<std::string>& target);//Hrc
+    //void go(const std::vector<std::string>& target); //Hrc
+    //void show_items(const std::vector<std::string>& target);//Hrc
     void look(const std::vector<std::string>& target); //xx
     void quit(const std::vector<std::string>& target); //xx
-
 
     std::map<std::string, Command> setup_commands();
     Location random_location();
